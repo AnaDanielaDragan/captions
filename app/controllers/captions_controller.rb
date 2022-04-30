cc97391945dcdf11348212f1523758b3c3d20b38
Ana <anadanieladragan@gmail.com>
2022-04-30 13:46:27 +0300

    GET /captions responds with static body

--- app/controllers/captions_controller.rb
+++ app/controllers/captions_controller.rb
@@ -2,6 +2,14 @@
 
 class CaptionsController < ApplicationController
   def index
-    head :ok
+    captions = [
+      {
+        id: 1,
+        url: "https://example.com/image.png",
+        text: "Caption on image",
+        caption_url: "https://localhost:3000/image.png"
+      }
+    ]
+    render json: { captions: captions }
   end
 end
