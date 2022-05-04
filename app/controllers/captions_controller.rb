60817a256ca5c1da552917a493507902ce90ea92
Ana <anadanieladragan@gmail.com>
2022-05-04 14:07:42 +0300

    Used Meme object for image handling

--- app/controllers/captions_controller.rb
+++ app/controllers/captions_controller.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require './lib/meme'
+
 class CaptionsController < ApplicationController
   def index
     captions = Caption.all
@@ -9,6 +11,14 @@ class CaptionsController < ApplicationController
 
   def create
     attributes = params.require(:caption).permit(:url, :text)
+
+    meme = Meme.new
+    meme.image_url = attributes[:url]
+    meme.text = attributes[:text]
+    meme.create
+
+    attributes[:caption_url] = Meme.file_path(meme.file_name)
+
     caption = Caption.create(attributes)
 
     render json: { caption: caption }, status: :created
