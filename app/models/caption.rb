597e892f75316ca2f1d4e3498188e19594127686
Ana <anadanieladragan@gmail.com>
2022-05-13 00:47:46 +0300

    Add validators to model

--- app/models/caption.rb
+++ app/models/caption.rb
@@ -1,4 +1,7 @@
 # frozen_string_literal: true
 
 class Caption < ApplicationRecord
+  validates :url, presence: true
+  validates :url, format: { with: URI.regexp, message: 'Invalid url format' }
+  validates :text, presence: true
 end
